6d6839d9177d00d8b19930eebd4b92e20547bc4e
Viktor1504 <viktordenisevich6@gmail.com>
2023-10-27 19:26:11 +0300

    add module css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 export const Profile = () => {
-    return <div className="content">
+    return <div className={s.content}>
         <div>
             <img
                 src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"/>
@@ -14,11 +15,11 @@ export const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
