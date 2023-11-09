5c86889f02f0fed99a01511c256f0232fec83dd0
Viktor1504 <viktordenisevich6@gmail.com>
2023-11-10 00:01:15 +0300

    add route, browser-router lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import {Navbar} from './components/navbar/Navbar';
 import {Profile} from './components/profile/Profile';
 import {Header} from './components/header/Header';
 import {Dialogs} from './components/dialogs/Dialogs';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className={'app-wrapper-content'}>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                    </Routes>
+                </div>
             </div>
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <NavLink to={'/profile'}>Profile</NavLink>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <NavLink to={'/dialogs'}>Messages</NavLink>
         </div>
         <div className={s.item}>
-            <a>News</a>
+            <NavLink to={'#'}>News</NavLink>
         </div>
         <div className={s.item}>
             <a>Music</a>
