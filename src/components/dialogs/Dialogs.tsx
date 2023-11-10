c2a3697f7f29cb4738fa9ffbe5cd1165a6ecc32f
Viktor1504 <viktordenisevich6@gmail.com>
2023-11-11 00:48:41 +0300

    add route for dialogs

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Dialogs = (props: any) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Victor
+                    <NavLink to={'dialogs/1'}>Victor </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Pavel
+                    <NavLink to={'dialogs/2'}>Pavel</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Dima
+                    <NavLink to={'dialogs/3'}>Dima</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Lena
+                    <NavLink to={'dialogs/4'}>Lena</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sveta
+                    <NavLink to={'dialogs/5'}>Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'dialogs/6'}>Valera</NavLink>
                 </div>
 
             </div>
